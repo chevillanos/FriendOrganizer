c40fe4e99f6700ae959d3db2ae19318248de7bf2
Carl Henry Villanos <cvillanos@racami.com>
2021-04-14 16:47:41 +0800

    Chapter 4 OK - build UI, MVVM binding pattern, use Autofac for DI

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Carl", LastName = "Villanos", Email = "carl@test.com" };
+            yield return new Friend { FirstName = "Henry", LastName = "Long", Email = "henry@test.com" };
+            yield return new Friend { FirstName = "Yoh", LastName = "Hoho", Email = "yohhoho@test.com" };
+            yield return new Friend { FirstName = "Ru", LastName = "Ffy", Email = "ruffy@test.com" };
+        }
+    }
+}
---
+++ FriendOrganizer.UI/Startup/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.ViewModel;
+
+namespace FriendOrganizer.UI
+{
+    public class Bootstrapper
+    {
+        public IContainer Bootstrap()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MainWindow>().AsSelf();
+            builder.RegisterType<MainViewModel>().AsSelf();
+            builder.RegisterType<FriendDataService>().As<IFriendDataService>();
+
+            return builder.Build();
+        }
+    }
+}
