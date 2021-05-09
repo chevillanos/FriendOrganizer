963088ade93bd7bfd0aefed75e7c84e1f9fe066b
Carl Henry Villanos <cvillanos@racami.com>
2021-05-10 04:41:32 +0800

    Chapter 6 complete - decoupling UI

---
+++ FriendOrganizer.Model/LookupItem.cs
@@ -0,0 +1,9 @@
+﻿namespace FriendOrganizer.Model
+{
+    // Generic class for returning a single entity
+    public class LookupItem
+    {
+        public int Id { get; set; }
+        public string DisplayMember { get; set; }
+    }
+}
---
+++ FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -0,0 +1,39 @@
+﻿using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class FriendDetailViewModel : ViewModelBase, IFriendDetailViewModel
+    {
+        public Friend Friend
+        {
+            get
+            {
+                return _friend;
+            }
+            private set
+            {
+                _friend = value;
+                OnPropertyChanged();
+            }
+        }
+        private Friend _friend;
+
+        private readonly IFriendDataService dataService;
+
+        public FriendDetailViewModel(IFriendDataService dataService)
+        {
+            this.dataService = dataService;
+        }
+
+        public async Task LoadAsync(int friendId)
+        {
+            Friend = await dataService.GetIdByAsync(friendId);
+        }
+    }
+}
