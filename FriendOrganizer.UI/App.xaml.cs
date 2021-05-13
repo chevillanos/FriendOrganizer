d5545ec6023635f7a62b16ce25798d186d9d6c11
Carl Villanos <cvillanos@racami.com>
2021-05-14 05:32:46 +0800

    Ch 9 - Lesson: Create the FriendWrapper in progress

--- FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer.UI/App.xaml.cs
@@ -29,5 +29,13 @@ namespace FriendOrganizer.UI
             var mainWindow = container.Resolve<MainWindow>();
             mainWindow.Show();
         }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Unexpected error occured. Please contact the admin." + Environment.NewLine +
+                e.Exception.Message, "Unexpected error");
+
+            e.Handled = true;
+        }
     }
 }
